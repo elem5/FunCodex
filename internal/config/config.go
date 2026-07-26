037354e0584423ecc9269217c03dffb788454e29
elem5 <281225875+elem5@users.noreply.github.com>
2026-07-26 02:18:21 +0200

    feat: add config file

---
+++ internal/config/config.go
@@ -0,0 +1,105 @@
+package config
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+)
+
+type Config struct {
+	AccentColor        string   `json:"accent_color"` // hex (e.g., "#FF5733")
+	ShowAscii          bool     `json:"show_ascii"`
+	SeparatorCharacter string   `json:"separator_character"`
+	Keybinds           Keybinds `json:"keybinds"`
+}
+
+type Keybinds struct {
+	EncryptMode string `json:"encrypt_mode"`
+	DecryptMode string `json:"decrypt_mode"`
+	Length      string `json:"length_mode"`
+}
+
+// default returns reasonable fallback settings if no config file exists yet.
+func Default() Config {
+	return Config{
+		AccentColor:        "#55559b",
+		ShowAscii:          true,
+		SeparatorCharacter: "|",
+		Keybinds: Keybinds{
+			EncryptMode: "ctrl+e",
+			DecryptMode: "ctrl+d",
+			Length:      "ctrl+l",
+		},
+	}
+}
+
+func Path() (string, error) {
+	dir, err := os.UserConfigDir()
+	if err != nil {
+		return "", err
+	}
+
+	return filepath.Join(
+		dir,
+		"funcodex",
+		"config.json",
+	), nil
+}
+
+func Load() (*Config, error) {
+	path, err := Path()
+	if err != nil {
+		return nil, err
+	}
+
+	// create directory if it doesn't exist
+	err = os.MkdirAll(
+		filepath.Dir(path),
+		0755,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// create default config file if missing
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		cfg := Default()
+
+		data, err := json.MarshalIndent(
+			cfg,
+			"",
+			"    ",
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		err = os.WriteFile(
+			path,
+			data,
+			0644,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		return &cfg, nil
+	}
+
+	// load existing config file
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	err = json.Unmarshal(
+		data,
+		&cfg,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
